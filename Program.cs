13e93834d1a67f11d52686845643d81cbef28e95
jrnjco <jrnjco@gmail.com>
2022-01-04 21:04:04 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conntecting to DB");
+            SqlConnection conObj = new SqlConnection(@"data source = DESKTOP-BEM16JA;database=AdventureWorks2012;Trusted_Connection=True;");
+            Console.WriteLine(conObj.State);
+            conObj.Open();
+            Console.WriteLine(conObj.State);
+        }
+    }
+}
